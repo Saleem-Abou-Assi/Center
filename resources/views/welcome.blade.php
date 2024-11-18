a7f5621145b606c19044da293362a46f3b5f65df
Ehab Albonay <ehab.bounnaye@gmail.com>
2024-11-18 11:56:25 +0300

    more stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
        <div class="card-container">
         <div class="card">
             <div class="icon">
-                <img src="" alt="card image" />
+                <img src="images/pat.jpeg" alt="card image" />
             </div>
             <div class="card-dis">
                 <h2>Patiants Page</h2>
@@ -38,39 +38,59 @@
        <div class="card-container">
         <div class="card">
             <div class="icon">
-                <img src="" alt="card image" />
+                <img src="images/doc.jpeg" alt="card image" />
             </div>
             <div class="card-dis">
-                <h2>Card Name</h2>
-                <p>card discription</p>
-                <button>Explore</button>
+                <h2>Doctors Page</h2>
+                <p>here yo can fund the doctors details</p>
+                <div class="boton">
+                <a href="{{ route('doctor.index')}}" class="cta"> <span>Go</span> 
+                <svg width="15px" height="10px" viewBox="0 0 13 10">    
+        <path d="M1,5 L11,5"></path>
+        <polyline points="8 1 12 5 8 9"></polyline>
+     </svg> </a>
+                </div>
             </div>
         </div>
        </div>
        <div class="card-container">
         <div class="card">
             <div class="icon">
-                <img src="" alt="card image" />
+                <img src="images/bok.jpeg" alt="card image" />
             </div>
             <div class="card-dis">
-                <h2>Card Name</h2>
-                <p>card discription</p>
-                <button>Explore</button>
+                <h2>Booking Page</h2>
+                <p>here you can find the Booking section</p>
+                <div class="boton">
+                <a href="{{ route('book.index')}}" class="cta"> <span>Go</span> 
+                <svg width="15px" height="10px" viewBox="0 0 13 10">    
+        <path d="M1,5 L11,5"></path>
+        <polyline points="8 1 12 5 8 9"></polyline>
+     </svg> </a>
+                </div>
             </div>
         </div>
        </div>
        <div class="card-container">
         <div class="card">
             <div class="icon">
-                <img src="" alt="card image" />
+                <img src="images/dep.jpeg" alt="card image" />
             </div>
             <div class="card-dis">
-                <h2>Card Name</h2>
-                <p>card discription</p>
-                <button>Explore</button>
+                <h2>Department Page</h2>
+                <p>here you can dind the department details</p>
+                <div class="boton">
+                <a href="{{ route('department.index')}}" class="cta"> <span>Go</span> 
+                <svg width="15px" height="10px" viewBox="0 0 13 10">    
+        <path d="M1,5 L11,5"></path>
+        <polyline points="8 1 12 5 8 9"></polyline>
+     </svg> </a>
+                </div>
             </div>
         </div>
        </div>
        </div>
+       <br>
+       <br>
     </body>
 </html>
