8430dd32da9f98cce7c746f07a5796d9f70c993a
odai Borasheed <odaiten@gmail.com>
2025-01-10 12:05:54 +0300

    added waiting list & images uploading for patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -8,6 +8,7 @@ use App\Models\Field;
 use App\Models\Patient;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PatientController extends Controller
 {
@@ -15,8 +16,8 @@ class PatientController extends Controller
     public function index()
     {
         $patients = Patient::paginate(200);
-        
-        return view('patient.index',['patients'=>$patients]);
+
+        return view('patient.index', ['patients' => $patients]);
     }
 
     public function create()
@@ -26,7 +27,6 @@ class PatientController extends Controller
 
     public function store(Request $request)
     {
-
         // تحقق من صحة الطلب
         $request->validate([
             'name' => ['required', 'string'],
@@ -34,49 +34,55 @@ class PatientController extends Controller
             'address' => ['required', 'string'],
             'gender' => ['required',],
             'age' => ['required', 'integer'],
-            
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
         ]);
+
+        $profileImagePath = null;
+
+        if ($request->hasFile('image')) {
+            $profileImagePath = $request->file('image')->store('patient_images', 'public');
+        }
 
         // إنشاء 
         $patient = Patient::create([
             'name' => $request->name,
-            'phone'=> $request->phone,
-            'address'=> $request->address,
-            'Gender'=> $request->gender,
-            'age'=> $request->age,
-            'job'=>$request->job,
-            'relation'=>$request->relation,
-            'childerCount'=>$request->children,
-            'smooking'=>$request->smooking,
-            'oldSurgery'=>$request->oldSurgery,
-            'alirgy'=>$request->alirgy,
-            'disease'=>$request->disease,
-            'dite'=>$request->dite,
-            'permenantCure'=>$request->permenantCure,
-            'Cosmetic'=>$request->cosmetic,
-            'CurrentDiseas'=>$request->currentDisease
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'Gender' => $request->gender,
+            'age' => $request->age,
+            'job' => $request->job,
+            'relation' => $request->relation,
+            'childerCount' => $request->children,
+            'smooking' => $request->smooking,
+            'oldSurgery' => $request->oldSurgery,
+            'alirgy' => $request->alirgy,
+            'disease' => $request->disease,
+            'dite' => $request->dite,
+            'permenantCure' => $request->permenantCure,
+            'Cosmetic' => $request->cosmetic,
+            'CurrentDiseas' => $request->currentDisease,
+            'profileImagePath' => $profileImagePath
         ]);
 
-         // Get the dynamic field names and values
-    $dynamicFieldNames = $request->input('dynamicFieldName');
-    $dynamicFieldValues = $request->input('dynamicFieldValue');
+        // Get the dynamic field names and values
+        $dynamicFieldNames = $request->input('dynamicFieldName');
+        $dynamicFieldValues = $request->input('dynamicFieldValue');
 
-    // Process the dynamic fields (e.g., save to the database)
-    if($dynamicFieldNames){
-    foreach ($dynamicFieldNames as $key => $fieldName) {
-        $fieldValue = $dynamicFieldValues[$key];
-        $field = Field::firstOrCreate(['name' => $fieldName, 'value' => $fieldValue]);
+        // Process the dynamic fields (e.g., save to the database)
+        if ($dynamicFieldNames) {
+            foreach ($dynamicFieldNames as $key => $fieldName) {
+                $fieldValue = $dynamicFieldValues[$key];
+                $field = Field::firstOrCreate(['name' => $fieldName, 'value' => $fieldValue]);
 
-        // Attach the field to the patient
-        $patient->Field()->attach($field);
-    }}
+                // Attach the field to the patient
+                $patient->Field()->attach($field);
+            }
+        }
 
         $account = Accounter::create([
-            'patient_id' => $patient->id,   
+            'patient_id' => $patient->id,
         ]);
 
-
-    
         return redirect()->route('patient.index'); // إعادة التوجيه بعد التخزين
     }
     public function edit($id)
@@ -85,87 +91,96 @@ class PatientController extends Controller
         return view('patient.create', ['patient' => $patient]);
     }
 
-    public function update(Request $request,$patient_id)
-         {
-            
-            $patient = Patient::where('id',$patient_id)->first();
-            
-            $request->validate([
-                'name' => ['required', 'string'],
-                'phone' => ['required', 'string'],
-                'address' => ['required', 'string'],
-                'gender' => ['required',],
-                'age' => ['required', 'integer'],
-                
-            ]);
- 
-            $patient->update([
-                'name' => $request->name,
-                'phone'=> $request->phone,
-                'address'=> $request->address,
-                'Gender'=> $request->gender,
-                'age'=> $request->age,
-                'job'=>$request->job,
-                'relation'=>$request->relation,
-                'childerCount'=>$request->children,
-                'smooking'=>$request->smooking,
-                'oldSurgery'=>$request->oldSurgery,
-                'alirgy'=>$request->aligry,
-                'disease'=>$request->disease,
-                'dite'=>$request->dite,
-                'permenantCure'=>$request->parmenantCure,
-                'Cosmetic'=>$request->cosmetic,
-                'CurrentDiseas'=>$request->currentDisease
-            ]);
+    public function update(Request $request, $patient_id)
+    {
 
-            $dynamicFieldNames = $request->input('dynamicFieldName');
-            $dynamicFieldValues = $request->input('dynamicFieldValue');
-        
+        $patient = Patient::where('id', $patient_id)->first();
 
-            if ($dynamicFieldNames) {
-                foreach ($dynamicFieldNames as $key => $fieldName) {
-                    $fieldValue = $dynamicFieldValues[$key];
-                    
-                    // Update or create the field
-                    $field = Field::updateOrCreate(
-                        ['name' => $fieldName], // Condition to check
-                        ['value' => $fieldValue] // Values to update or create
-                    );
-            $field->save();
-                    // Attach the field to the patient
-                    $patient->Field()->syncWithoutDetaching([$field->id]);
-                }
+        $request->validate([
+            'name' => ['required', 'string'],
+            'phone' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'gender' => ['required',],
+            'age' => ['required', 'integer'],
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
+        ]);
+
+        $profileImagePath = null;
+
+        if ($request->hasFile('image')) {
+            // Store the image in the 'public/patient_images' directory
+            $profileImagePath = $request->file('image')->store('patient_images', 'public');
+        }
+
+        $patient->update([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'Gender' => $request->gender,
+            'age' => $request->age,
+            'job' => $request->job,
+            'relation' => $request->relation,
+            'childerCount' => $request->children,
+            'smooking' => $request->smooking,
+            'oldSurgery' => $request->oldSurgery,
+            'alirgy' => $request->aligry,
+            'disease' => $request->disease,
+            'dite' => $request->dite,
+            'permenantCure' => $request->parmenantCure,
+            'Cosmetic' => $request->cosmetic,
+            'CurrentDiseas' => $request->currentDisease,
+            'profileImagePath' => $profileImagePath
+        ]);
+
+        $dynamicFieldNames = $request->input('dynamicFieldName');
+        $dynamicFieldValues = $request->input('dynamicFieldValue');
+
+
+        if ($dynamicFieldNames) {
+            foreach ($dynamicFieldNames as $key => $fieldName) {
+                $fieldValue = $dynamicFieldValues[$key];
+
+                // Update or create the field
+                $field = Field::updateOrCreate(
+                    ['name' => $fieldName], // Condition to check
+                    ['value' => $fieldValue] // Values to update or create
+                );
+                $field->save();
+                // Attach the field to the patient
+                $patient->Field()->syncWithoutDetaching([$field->id]);
             }
-            
-            
-             
-             return redirect()->route('patient.index'); 
-            }
+        }
 
-     public function destroy($patient_id)
-     { 
+        return redirect()->route('patient.index');
+    }
+
+    public function destroy($patient_id)
+    {
         try {
             $patient = Patient::where('id', $patient_id)->first();
-    
+            if ($patient->image_path) {
+                Storage::delete('public/' . $patient->image_path);
+            }
             if ($patient) {
                 $patient->delete();
                 session()->flash('success', 'Patient deleted successfully.');
             } else {
                 session()->flash('error', 'Patient not found.');
             }
-    
+
             return redirect()->route('patient.index');
         } catch (Exception $e) {
             session()->flash('error', 'you can\' delete patient because he has stored data.');
             return redirect()->route('patient.index');
         }
-}
+    }
 
-    public function show($patient_id) {
-        $patient = Patient::with('Dept','Field','Lazer.doctor','accounter')->find($patient_id);
-        
-        $account = Accounter::where('patient_id',$patient_id)->first();
-        $apds = APD::where('A_id',$account->id)->get();
-        return view('patient.show', ['patient'=>$patient,'apds'=>$apds]); 
+    public function show($patient_id)
+    {
+        $patient = Patient::with('Dept', 'Field', 'Lazer.doctor', 'accounter')->find($patient_id);
+
+        $account = Accounter::where('patient_id', $patient_id)->first();
+        $apds = APD::where('A_id', $account->id)->get();
+        return view('patient.show', ['patient' => $patient, 'apds' => $apds]);
     }
 }
--- app/Http/Controllers/WaitingListController.php
+++ app/Http/Controllers/WaitingListController.php
@@ -4,15 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Doctor;
 use App\Models\Patient;
+use App\Models\WaitingList;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WaitingListController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $doctors = Doctor::all();
         $patients = Patient::all();
-        return view('waitingList.index',['doctors'=>$doctors,'patients'=>$patients]);
+        $selectedDoctor = null;
+        if ($request->has('doctor_id')) {
+            $selectedDoctor = Doctor::find($request->doctor_id);
+        }
+
+        return view('waitingList.index', ['doctors' => $doctors, 'patients' => $patients, 'selectedDoctor' => $selectedDoctor]);
     }
+
+
+    public function store(Request $request)
+    {
+       
+        $doctor = Doctor::find($request->doctor_id);
+        $patient = Patient::find($request->patient_id);
+
+        $doctor->waitinglist()->attach($patient->id, [
+            'expires_at' => now()->addDays(1),
+        ]);
+
+        return redirect()->back()->with('success', 'Patient added to the waiting list.');
+    }
+
+    public function destroy($entryId)
+{
+    $entry = WaitingList::find($entryId);
+    $entry->delete();
+
+    return redirect()->back()->with('success', 'Patient removed from the waitlist.');
+}
 
 }
