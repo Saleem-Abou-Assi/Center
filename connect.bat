efb019314012cae65d7ea53e275fe9f8c4aa2c7c
Ehab Albonay <ehab.bounnaye@gmail.com>
2025-01-14 13:14:34 +0300

    rrgw

---
+++ connect.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+REM Set the static IP address of the machine running the Laravel application
+set IP=192.168.1.1  REM Replace with your actual static IP address
+
+REM Set the URL of your localhost site
+set URL=http://%IP%:8000/
+
+REM Open the URL in Chrome
+start chrome --new-window --app="%URL%" || start chrome --start-fullscreen "%URL%"
