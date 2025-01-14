76c39ad1e857d2991a3d51ca3074a167d3337575
Saleem-Abou-Assi <amhadi696@gmail.com>
2025-01-14 13:35:57 +0300

    asdas

--- run.bat
+++ run.bat
@@ -1,6 +1,17 @@
 @echo off
 setlocal
 
+
+REM Define database file and backup path
+set DB_FILE=C:\Users\AboHadi\Desktop\Center\Center\database\database.sqlite
+set BACKUP_PATH=C:\Users\AboHadi\Desktop\Center\Backup\database_backup.sqlite
+
+REM Backup the SQLite database
+echo Backing up the SQLite database...
+copy "%DB_FILE%" "%BACKUP_PATH%"
+
+
+
 REM Get the IPv4 address
 for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /i "IPv4"') do (
     for /f "tokens=1 delims= " %%b in ("%%a") do (
